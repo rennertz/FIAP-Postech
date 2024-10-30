4468ced2e6be4a857863916c1a67df4eabcbedc4
Renner <renner.xd@gmail.com>
2024-10-30 20:06:02 -0300

    incrementa AtendimentoService

--- Fase 1 Tech Challenge/src/main/java/br/com/vetvision/supervisor/application/AtendimentoService.java
+++ Fase 1 Tech Challenge/src/main/java/br/com/vetvision/supervisor/application/AtendimentoService.java
@@ -1,9 +1,16 @@
 package br.com.vetvision.supervisor.application;
 
+import br.com.vetvision.supervisor.domain.model.exame.Exame;
+import br.com.vetvision.supervisor.domain.model.oferta.OfertaAtendimento;
 import br.com.vetvision.supervisor.domain.model.solicitacao.Solicitacao;
 
 public interface AtendimentoService {
 
-    Solicitacao solicitarExame();
+    Solicitacao solicitarExame(Solicitacao solicitacao);
 
+    OfertaAtendimento ofertarAtendimento(OfertaAtendimento ofertaAtendimento);
+    Exame aceitarOferta(OfertaAtendimento ofertaAtendimento);
+
+    Exame realizarExame(Exame exame);
+    Exame encaminharLaudo(Exame exame);
 }
