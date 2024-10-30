aeb6a70628870c73d6eddcb6d2003cce277c3064
Renner <renner.xd@gmail.com>
2024-10-30 20:45:30 -0300

    solicitarExameTest falhando (TDD)

--- Fase 1 Tech Challenge/src/main/java/br/com/vetvision/supervisor/domain/model/solicitacao/Solicitacao.java
+++ Fase 1 Tech Challenge/src/main/java/br/com/vetvision/supervisor/domain/model/solicitacao/Solicitacao.java
@@ -6,11 +6,29 @@ import br.com.vetvision.supervisor.domain.model.oferta.OfertaAtendimento;
 import java.time.LocalDateTime;
 
 public class Solicitacao {
-    private int id;
+    private Integer id;
     private Clinica clinica;
     private Pet pet;
     private PlanoVeterinario plano;
     private LocalDateTime momentoCriacao;
 
     private OfertaAtendimento ofertaAtual;
+
+    public Solicitacao(Clinica clinica, Pet pet, PlanoVeterinario plano) {
+        this.clinica = clinica;
+        this.pet = pet;
+        this.plano = plano;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public LocalDateTime getMomentoCriacao() {
+        return momentoCriacao;
+    }
+
+    public OfertaAtendimento getOfertaAtual() {
+        return ofertaAtual;
+    }
 }
