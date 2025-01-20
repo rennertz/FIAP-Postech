0d1e331d66bc52f8acd093d3ae4d38cda5e1e559
renner <renner.xd@gmail.com>
2025-01-20 20:27:33 -0300

    configura cache redis para veículos estacionados

--- fase2-parquimetro/src/main/java/br/com/pixpark/parquimetro/infrastructure/RedisConfig.java
+++ fase2-parquimetro/src/main/java/br/com/pixpark/parquimetro/infrastructure/RedisConfig.java
@@ -1,11 +1,14 @@
 package br.com.pixpark.parquimetro.infrastructure;
 
 import br.com.pixpark.parquimetro.domain.model.Bilhete;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericToStringSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -14,9 +17,22 @@ public class RedisConfig {
     @Bean
     RedisTemplate<String, Bilhete> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Bilhete> template = new RedisTemplate<>();
+
+        // Configuração do ObjectMapper
+        ObjectMapper objectMapper = new ObjectMapper()
+                .registerModule(new JavaTimeModule())
+                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        // Criar o serializador configurado com o ObjectMapper
+        Jackson2JsonRedisSerializer<Bilhete> valueSerializer = new Jackson2JsonRedisSerializer<>(objectMapper, Bilhete.class);
+
+        // Configurar os serializadores
         template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new GenericToStringSerializer<>(Bilhete.class));
+        template.setValueSerializer(valueSerializer);
+
+        // Configurar a conexão
         template.setConnectionFactory(connectionFactory);
+
         return template;
     }
 }
