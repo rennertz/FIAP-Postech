fe376a76b58138c3efc5ed1c71da3aaed92e969b
renner <renner.xd@gmail.com>
2025-03-15 23:45:34 -0300

    testa useCase, corrige envs

--- fase3-booknrest/src/test/java/br/com/booknrest/booknrest/application/CadastroDeRestauranteUseCaseTest.java
+++ fase3-booknrest/src/test/java/br/com/booknrest/booknrest/application/CadastroDeRestauranteUseCaseTest.java
@@ -1,9 +1,17 @@
 package br.com.booknrest.booknrest.application;
 
+import br.com.booknrest.booknrest.entities.Restaurante;
+import br.com.booknrest.booknrest.exceptions.ErroDeValidacao;
 import br.com.booknrest.booknrest.gateway.RestauranteGateway;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 
 
 class CadastroDeRestauranteUseCaseTest {
@@ -20,9 +28,24 @@ class CadastroDeRestauranteUseCaseTest {
 
     @Test
     void salvaRestaurante() {
+        when(gatewayMock.cadastra(getRestaurante())).thenReturn(getRestaurante());
+        when(gatewayMock.obtemPeloNome(anyString())).thenReturn(Optional.empty());
+
+        assertDoesNotThrow(() ->
+                cadastroDeRestaurante.salvaRestaurante(getRestaurante())
+        );
     }
 
     @Test
-    void obtemRestaurantes() {
+    void restauranteJaCadastrado() {
+        when(gatewayMock.obtemPeloNome(anyString())).thenReturn(Optional.of(getRestaurante()));
+
+        assertThrows(ErroDeValidacao.class, () ->
+            cadastroDeRestaurante.salvaRestaurante(getRestaurante())
+        );
+    }
+
+    private static Restaurante getRestaurante() {
+        return new Restaurante(1L, "Moqueca", "Mooca", "frutos do mar", 150);
     }
 }
